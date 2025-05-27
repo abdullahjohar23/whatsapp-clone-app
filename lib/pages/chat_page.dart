712a7029ed1c4280dc097777d81c88cf81eaaec9
Abdullah Johar <jrrabdullah23@gmail.com>
2025-05-27 20:06:36 +0600

    chat page first commit

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clon_app/pages/single_inbox_design.dart';
+
+class ChatPage extends StatefulWidget {
+    const ChatPage({super.key});
+
+    @override
+    State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+    late double _deviceHeight, _deviceWidth;
+
+    @override
+    Widget build(BuildContext context) {
+        _deviceHeight = MediaQuery.of(context).size.height;
+        _deviceWidth = MediaQuery.of(context).size.width;
+
+        return Scaffold(
+            backgroundColor: Color(0xfffefefe),
+
+            //* APpBar Design
+            appBar: AppBar(
+                backgroundColor: Color(0xfffefefe),
+
+                title: Padding(
+                    padding: EdgeInsets.only(top: _deviceHeight/55),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,                  
+                        children: [
+                            Text(
+                                'WhatsApp',
+                                style: TextStyle(
+                                    color: Color(0xff1dab61),
+                                    fontSize: _deviceWidth/15,
+                                    fontWeight: FontWeight.bold
+                                ),
+                            ),
+                    
+                            Row(
+                                children: [
+                                    Icon(Icons.camera_alt_outlined, size: _deviceWidth/15),
+                                    SizedBox(width: _deviceWidth/20),
+                                    Icon(Icons.search, size: _deviceWidth/15),
+                                    SizedBox(width: _deviceWidth/20),
+                                    Icon(Icons.more_vert, size: _deviceWidth/15),
+                                ],
+                            ),
+                        ],
+                    ),
+                ),
+
+                bottom: PreferredSize(
+                    preferredSize: Size.fromHeight(_deviceHeight*0.01), // Height of the line
+                    child: Container(
+                        color: Colors.grey[300],         // Light gray line
+                        height: 1,
+                    ),
+                ),
+            ),
+
+            //* Body Section
+            body: Container(
+                margin: EdgeInsets.only(left: _deviceWidth*0.045, right: _deviceWidth*0.045, top: _deviceHeight*0.04),
+                child: Column(
+                    children: [
+                        //* this section is design for single chat and it will be repeated for other chats
+                        ChatTile(imagePath: 'assets/ammu.png', name: 'Ammu', time: 'Yesterday', message: '2 kg alu aan forhad er dokan theke. boro beche anis'),
+                        ChatTile(imagePath: 'assets/faysal.png', name: 'Faysal', time: 'Yesterday', message: 'Kire, ajke ber hobi na?'),
+                        ChatTile(imagePath: 'assets/emon.png', name: 'Emon', time: '1:17 PM', message: 'Gorur hut e jabi kalke?'),
+                        ChatTile(imagePath: 'assets/taif.png', name: 'Taif', time: '5/24/25', message: 'CPS Academy er course ta niba naki dekho'),
+                        ChatTile(imagePath: 'assets/samiul.png', name: 'Samiul', time: '5/18/25', message: '2 kg alu aan forhad er dokan theke. boro beche anis'),
+                        ChatTile(imagePath: 'assets/sunday.png', name: 'Only Sunday', time: '4/22/25', message: 'Faysal: 8tay ber hobo ajke'),
+                    ],
+                ),
+            ),
+        );
+    }
+}
