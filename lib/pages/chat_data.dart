9107acb3d2a10b87e572b04c1f8697fca82a1968
Abdullah Johar <jrrabdullah23@gmail.com>
2025-05-28 19:46:35 +0600

    chat data page for inbox information

---
+++ lib/pages/chat_data.dart
@@ -0,0 +1,69 @@
+// chat_data.dart
+final List<Map<String, String>> chatData = [
+  {
+    'imagePath': 'assets/ammu.png',
+    'name': 'Ammu',
+    'time': 'Yesterday',
+    'message': '2 kg alu aan forhad er dokan theke. boro beche anis',
+  },
+  {
+    'imagePath': 'assets/faysal.png',
+    'name': 'Faysal',
+    'time': 'Yesterday',
+    'message': 'Kire, ajke ber hobi na?',
+  },
+  {
+    'imagePath': 'assets/emon.png',
+    'name': 'Emon',
+    'time': '1:17 PM',
+    'message': 'Gorur hut e jabi kalke?',
+  },
+  {
+    'imagePath': 'assets/taif.png',
+    'name': 'Taif',
+    'time': '5/24/25',
+    'message': 'CPS Academy er course ta niba naki dekho',
+  },
+  {
+    'imagePath': 'assets/samiul.png',
+    'name': 'Samiul IIUC',
+    'time': '5/18/25',
+    'message': 'Brain Station e apply kor',
+  },
+  {
+    'imagePath': 'assets/tanjid.jpg',
+    'name': 'Tanjid IIUC',
+    'time': '5/17/25',
+    'message': 'Brain Station e apply kore kichu hobe na',
+  },
+  {
+    'imagePath': 'assets/sunday.png',
+    'name': 'Only Sunday',
+    'time': '4/22/25',
+    'message': 'Faysal: 8tay ber hobo ajke',
+  },
+  {
+    'imagePath': 'assets/azim.jpg',
+    'name': 'Azim IIUC',
+    'time': '4/20/25',
+    'message': 'Ekta routine banai dis to bondhu',
+  },
+  {
+    'imagePath': 'assets/alauddin.jpg',
+    'name': 'Alauddin IIUC',
+    'time': '4/17/25',
+    'message': 'Algo te abar retake khaisi beda',
+  },
+  {
+    'imagePath': 'assets/baizid.jpg',
+    'name': 'Baizid Junior',
+    'time': '4/13/25',
+    'message': 'Bhaiya biye hoye gese amar',
+  },
+  {
+    'imagePath': 'assets/nahid.jpg',
+    'name': 'Nahid IIUC',
+    'time': '4/13/25',
+    'message': 'Beda, biye arekta korbo',
+  },
+];
